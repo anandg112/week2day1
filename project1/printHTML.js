96fe8c4b2010d4ab6f1ea2da90e2b1de839b5c0b
Anand <anandg112@gmail.com>
2017-02-13 16:30:05 +0000

    reading data chunk at a time

---
+++ project1/printHTML.js
@@ -0,0 +1,28 @@
+var https = require('https');
+var data = '';
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  /* Add your code here */
+  https.get(requestOptions, function(resp){
+    resp.setEncoding('utf8');
+
+    resp.on('data', function(chunk){
+      data += chunk;
+      console.log(data + '\n');
+    });
+
+    resp.on('end', function(){
+      console.log('Response stream completed');
+    });
+
+  });
+
+}
+
+getAndPrintHTML();
