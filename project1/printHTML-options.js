cff3bb01d04de0a81b0b53391199d7d4ffd2d9e2
Anand <anandg112@gmail.com>
2017-02-13 16:56:52 +0000

    Reading URL from a object

---
+++ project1/printHTML-options.js
@@ -0,0 +1,31 @@
+var https = require('https');
+var data = '';
+
+
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+
+  https.get(requestOptions, function(resp){
+    resp.setEncoding('utf8');
+
+    resp.on('data', function(chunk){
+      data += chunk;
+      console.log(data + '\n');
+    });
+
+    resp.on('end', function(){
+      console.log('Response stream completed');
+    });
+
+  });
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
+
