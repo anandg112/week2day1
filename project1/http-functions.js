4c78766a77e96b71164d363e42f996386137538a
Anand <anandg112@gmail.com>
2017-02-13 18:46:47 +0000

    added module and tested it

---
+++ project1/http-functions.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+function printHTML (html) {
+  console.log(html);
+};
+
+function getHTML (options, callback) {
+    /* Your code here */
+
+    var data = '';
+
+    https.get(options, function(resp){
+    resp.setEncoding('utf8');
+
+    resp.on('data', function(chunk){
+      data += chunk;
+      return callback(data);
+    });
+
+    resp.on('end', function(){
+      console.log('Response stream completed');
+    });
+
+  });
+
+};
+
+
+module.exports = {
+  getHTML: getHTML,
+  printHTML: printHTML
+};
+
